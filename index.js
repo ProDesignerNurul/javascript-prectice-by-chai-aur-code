c656863905193377a650df050cdc27a09510f328
ProDesignerNurul <maxnewsbd@gmail.com>
2024-02-25 18:53:21 +0600

    prectice variable, data type

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+const myName = 'md nurul amin';
+console.log(myName);
+const myAge = 28;
+const myAddress = 'gohorpur, balagonj, sylhet';
+console.log(myName, myAge, myAddress);
+console.table([myName,myAge, myAddress]);
+
+// 3 type of data type 
+// 1. (one) String
+const friendName = 'MD Nurul Amin';
+// 2 (tow) number
+const herAge = 25;
+// 3 three boolean 
+const isGood = true;
+
+// output 
+console.table([friendName, herAge, isGood]);
